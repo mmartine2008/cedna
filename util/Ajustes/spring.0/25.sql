2d15844d6622cd95ca36d85984abe86abbe81e75
Juan ignacio Martel sanchez <juanom07@MacBook-Air-de-Juan-2.local>
2019-03-01 17:32:14 -0300

    Ajuste a la base

---
+++ util/Ajustes/spring.0/25.sql
@@ -0,0 +1,6 @@
+UPDATE cedna.dbo.OperacionAccionPerfil
+SET IdOperacion=32, IdAccion=2, IdPerfil=3, jsFunction='updateInput()', urlDestino='', ordenUbicacion=9, idHTMLElement='botonGuardar'
+WHERE Id=87;
+
+
+INSERT INTO ajustes(script, diahora, spring, fix) VALUES ('25.sql', GETDATE ( ), 0, 25);
