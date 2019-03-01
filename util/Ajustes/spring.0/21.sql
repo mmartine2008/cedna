26468e8b442a4d6dd43f707477b5a629cf3d0be5
Micaela <mica@MacBook-Pro-de-Micaela.local>
2019-03-01 17:27:00 -0300

    avances en respuesta generada

--- util/Ajustes/spring.0/21.sql
+++ util/Ajustes/spring.0/21.sql
@@ -13,19 +13,19 @@ SET Funcion='getActividadesObra'
 WHERE IdPregunta=7 GO
 UPDATE cedna.dbo.Pregunta
 SET Funcion='getElementosProteccionPersonal'
-WHERE IdPregunta=8 GO
+WHERE IdPregunta=8;
 UPDATE cedna.dbo.Pregunta
 SET Funcion='getRiesgosAmbientales'
-WHERE IdPregunta=9 GO
+WHERE IdPregunta=9;
 UPDATE cedna.dbo.Pregunta
 SET Funcion='getRiesgosAdicionales'
-WHERE IdPregunta=10 GO
+WHERE IdPregunta=10;
 UPDATE cedna.dbo.Pregunta
 SET Funcion='getPruebaDeGases'
-WHERE IdPregunta=14 GO
+WHERE IdPregunta=14;
 UPDATE cedna.dbo.Pregunta
 SET Funcion='getFirmasPermiso'
-WHERE IdPregunta=15 GO
+WHERE IdPregunta=15;
 
 
 INSERT INTO ajustes(script, diahora, spring, fix) VALUES ('21.sql', GETDATE ( ), 0, 21);
