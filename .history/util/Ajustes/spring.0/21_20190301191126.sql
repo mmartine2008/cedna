c94a5d3b16f5e5044b901b38c75d31c7578065c2
Micaela <mica@MacBook-Pro-de-Micaela.local>
2019-03-06 15:24:08 -0300

    .

---
+++ .history/util/Ajustes/spring.0/21_20190301191126.sql
@@ -0,0 +1,32 @@
+-- Actual parameter values may differ, what you see is a default string representation of values
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getEmpresas'
+WHERE IdPregunta=3 ;
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getLugarObra'
+WHERE IdPregunta=5 ;
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getEtapasObra'
+WHERE IdPregunta=6 ;
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getActividadesObra'
+WHERE IdPregunta=7 ;
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getElementosProteccionPersonal'
+WHERE IdPregunta=8 ;
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getRiesgosAmbientales'
+
+WHERE IdPregunta=9 ;
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getRiesgosAdicionales'
+WHERE IdPregunta=10 ;
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getPruebaDeGases'
+WHERE IdPregunta=14 ;
+UPDATE cedna.dbo.Pregunta
+SET Funcion='getFirmasPermiso'
+WHERE IdPregunta=15 ;
+
+
+INSERT INTO ajustes(script, diahora, spring, fix) VALUES ('21.sql', GETDATE ( ), 0, 21);
