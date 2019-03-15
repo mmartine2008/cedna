7c1f95c0c317e941039a8cf0fbef21a59d8107a9
Juan ignacio Martel sanchez <juanom07@MacBook-Air-de-Juan-2.local>
2019-03-15 16:25:18 -0300

    modifica script sql para los nodos del organigrama

--- util/Ajustes/spring.0/36.sql
+++ util/Ajustes/spring.0/36.sql
@@ -30,4 +30,35 @@ ADD CONSTRAINT FK_Relevamientos_EstadosRelevamientos FOREIGN KEY (IdEstadoReleva
 UPDATE Relevamientos
 SET IdEstadoRelevamiento = 1;
 
+SET IDENTITY_INSERT cedna.dbo.Nodos ON;
+
+--INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+--VALUES(1, 2, 'Planta Principal FEMSA', NULL);
+
+INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+VALUES(4, 2, 'Administración', 1);
+
+INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+VALUES(5, 2, 'Producción', 1);
+
+INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+VALUES(6, 3, 'Logística', 1);
+
+INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+VALUES(7, 2, 'Recursos Humanos', 1);
+
+INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+VALUES(8, 2, 'Contabilidad', 4);
+
+INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+VALUES(9, 3, 'Personal', 4);
+
+INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+VALUES(10, 2, 'Ventas', 5);
+
+INSERT INTO cedna.dbo.Nodos (IdNodo, IdTipoNodo, Nombre, IdNodoSuperior)
+VALUES(11, 3, 'Distribución', 5);
+
+SET IDENTITY_INSERT cedna.dbo.Nodos OFF;
+
 INSERT INTO ajustes(script, diahora, spring, fix) VALUES ('36.sql', GETDATE ( ), 0, 36);
