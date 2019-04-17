785e439b4d66041dd5249f72631e996a424a01e8
Juan Ignacio Martel Sanchez <juano@MacBook-Pro-de-Juan.local>
2019-04-17 11:19:41 -0300

    Ajustes para correcciones despues de la demo

--- util/Ajustes/spring.1/14.sql
+++ util/Ajustes/spring.1/14.sql
@@ -9,10 +9,29 @@ DELETE FROM app.Pregunta WHERE IdPregunta IN (2, 5, 6);
 SET IDENTITY_INSERT cedna.app.OperacionAccionPerfil ON;
 
 INSERT INTO cedna.app.OperacionAccionPerfil (Id, IdOperacion, IdAccion, IdPerfil, jsFunction, urlDestino, ordenUbicacion, idHTMLElement)
---VALUES(145, 45, 1, 1, '', 'formulario', 2, ''); CAMBIAR Id
+VALUES(1258, 45, 1, 1, '', 'formulario', 2, '');
 
 INSERT INTO cedna.app.OperacionAccionPerfil (Id, IdOperacion, IdAccion, IdPerfil, jsFunction, urlDestino, ordenUbicacion, idHTMLElement)
---VALUES(146, 45, 7, 1, '', 'logout', 11, ''); CAMBIAR Id
+VALUES(1259, 45, 7, 1, '', 'logout', 11, '');
+
+INSERT INTO cedna.app.OperacionAccionPerfil (Id, IdOperacion, IdAccion, IdPerfil, jsFunction, urlDestino, ordenUbicacion, idHTMLElement)
+VALUES(1260, 40, 1, 5, '', 'formulario', 2, '');
+
+INSERT INTO cedna.app.OperacionAccionPerfil (Id, IdOperacion, IdAccion, IdPerfil, jsFunction, urlDestino, ordenUbicacion, idHTMLElement)
+VALUES(1261, 40, 7, 5, '', 'logout', 11, '');
+
+INSERT INTO cedna.app.OperacionAccionPerfil (Id, IdOperacion, IdAccion, IdPerfil, jsFunction, urlDestino, ordenUbicacion, idHTMLElement)
+VALUES(1262, 40, 6, 5, 'preEditar()', '', 6, 'botonEditar');
+
+INSERT INTO cedna.app.OperacionAccionPerfil (Id, IdOperacion, IdAccion, IdPerfil, jsFunction, urlDestino, ordenUbicacion, idHTMLElement)
+VALUES(1263, 32, 1, 5, '', 'formulario', 2, '');
+
+INSERT INTO cedna.app.OperacionAccionPerfil (Id, IdOperacion, IdAccion, IdPerfil, jsFunction, urlDestino, ordenUbicacion, idHTMLElement)
+VALUES(1264, 32, 7, 5, '', 'logout', 11, '');
+
+INSERT INTO cedna.app.OperacionAccionPerfil (Id, IdOperacion, IdAccion, IdPerfil, jsFunction, urlDestino, ordenUbicacion, idHTMLElement)
+VALUES(1265, 32, 2, 5, 'updateInput()', '', 9, 'botonGuardar');
 
 SET IDENTITY_INSERT cedna.app.OperacionAccionPerfil OFF;
+
 INSERT INTO ajustes(script, diahora, spring, fix) VALUES ('13.sql', GETDATE ( ), 1, 13);
