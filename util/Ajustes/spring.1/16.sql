f5f8dad42c96925eda823d1b79105d5ee047589a
Juan Ignacio Martel Sanchez <juano@MBP-de-Juan.utopia.net>
2019-04-28 11:18:07 -0300

    Ajustes de nuevos iconos de ordenes de compras y planificaciones

---
+++ util/Ajustes/spring.1/16.sql
@@ -0,0 +1,9 @@
+UPDATE cedna.app.Operacion
+SET icono='ordenesCompras zoom'
+WHERE Id=33;
+
+UPDATE cedna.app.Operacion
+SET icono='planificacion zoom'
+WHERE Id=36
+
+INSERT INTO ajustes(script, diahora, spring, fix) VALUES ('16.sql', GETDATE ( ), 1, 16);
