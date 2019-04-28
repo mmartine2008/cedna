8104475bee759883a8a2f2d1fc9ccfe5fb9059c7
Juan Ignacio Martel Sanchez <juano@MBP-de-Juan.utopia.net>
2019-04-28 11:18:34 -0300

    Ajuste en script

--- util/Ajustes/spring.1/16.sql
+++ util/Ajustes/spring.1/16.sql
@@ -4,6 +4,6 @@ WHERE Id=33;
 
 UPDATE cedna.app.Operacion
 SET icono='planificacion zoom'
-WHERE Id=36
+WHERE Id=36;
 
 INSERT INTO ajustes(script, diahora, spring, fix) VALUES ('16.sql', GETDATE ( ), 1, 16);
