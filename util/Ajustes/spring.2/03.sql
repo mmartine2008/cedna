4f2b16d38e2980189c658150d698f2ac9df252a9
Juan Ignacio Martel Sanchez <juano@MacBook-Pro-de-Juan.local>
2019-10-02 15:37:40 -0300

    Ajustes de iconos

--- util/Ajustes/spring.2/03.sql
+++ util/Ajustes/spring.2/03.sql
@@ -3,3 +3,13 @@ SET Funcion='getRiesgosAdicionales' WHERE IdPregunta=10;
 
 UPDATE app.Pregunta
 SET Funcion='getRiesgosAdicionales' WHERE IdPregunta=15;
+
+UPDATE app.Operacion
+SET icono='herramientas-planificacion zoom'
+WHERE Id=1076;
+
+UPDATE app.Operacion
+SET icono='operarios-planificacion zoom'
+WHERE Id=1078;
+
+INSERT INTO ajustes(script, diahora, spring, fix) VALUES ('03.sql', GETDATE ( ), 2, 03);
